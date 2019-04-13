e7f71b91235ca4528fef8ffbb3c44e345ec6f331
Ers28203006 <ersyn_sultanbaev@mail.ru>
2019-04-14 03:50:19 +0600

    Добавьте файлы проекта.

---
+++ SalesDb.WinFormsApp/Form1.cs
@@ -0,0 +1,58 @@
+﻿using SalesDb.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesDb.WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Customers(Покупатели)")
+            {
+                listBox1.Items.Clear();
+                CustomerTableQuery.Show(listBox1);
+
+            }
+            else if(comboBox1.Text == "Sellers(Продавцы)")
+            {
+                listBox1.Items.Clear();
+                SellersTableQuery.Show(listBox1);
+            }
+            else
+            {
+                listBox1.Items.Clear();
+                SalesTableQuery.Show(listBox1);
+            }
+        }
+    }
+}
